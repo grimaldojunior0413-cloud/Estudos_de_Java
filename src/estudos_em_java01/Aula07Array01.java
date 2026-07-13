809271a7f6eaac65072302ee88e2f0dca919396b
Grimaldo <grimaldojunior04@gmail.com>
2026-07-13 10:30:33 -0300

    introdução ao Arrys

---
+++ src/estudos_em_java01/Aula07Array01.java
@@ -0,0 +1,15 @@
+package estudos_em_java01;
+
+public class Aula07Array01 {
+    // arrays são referencias
+    public static void main(String[] args) {
+        int [] idades = new int[3];
+        idades [0] = 21;
+        idades [1] = 15;
+        idades [2] = 11;
+        idades [3] = 11;
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+    }
+}
