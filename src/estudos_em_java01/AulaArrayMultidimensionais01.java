289eec098a21e67fe63c86e93464edb26fe550bd
Grimaldo <grimaldojunior04@gmail.com>
2026-07-17 21:57:32 -0300

    error: switch m' requires a value

---
+++ src/estudos_em_java01/AulaArrayMultidimensionais01.java
@@ -0,0 +1,23 @@
+package estudos_em_java01;
+
+public class AulaArrayMultidimensionais01 {
+    public static void main(String[] args) {
+        // 1,2,3,4,5 Meses
+        // 31,28,31,30 dias
+        int[] [] dias = new int[3] [3];
+        //int[] dias = new int[3];
+        dias [0][0]= 31;
+        dias [0][1]= 28;
+        dias [0][2]= 31;
+
+        dias [1][0]= 31;
+        dias [1][1]= 28;
+        dias [1][2]= 31;
+
+        for (int i = 0 ; i< dias.length; i++){
+            for (int j = 0; j < dias[0].length; j++){
+                System.out.println(dias[i][j]);
+            }
+        }
+    }
+}
