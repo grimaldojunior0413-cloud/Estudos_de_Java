6e6306a5f02b57be143a1803b891fa203f364434
Grimaldo <grimaldojunior04@gmail.com>
2026-07-19 23:20:57 -0300

    exercicio usando for, contagem regresiva

---
+++ src/estudos_em_java01/Exercicios07.java
@@ -0,0 +1,10 @@
+package estudos_em_java01;
+
+public class Exercicios07 {
+    //Escreva um programa que faça uma contagem regressiva de 10 a 0. Use um laço for para a contagem.
+    public static void main(String[] args) {
+        for (int i = 0; i<=10; i++){
+            System.out.println(i);
+        }
+    }
+}
