20a67725fc7cf7ca7506fcbc4082b2ee8dfb0888
Grimaldo <grimaldojunior04@gmail.com>
2026-07-19 23:17:52 -0300

    exercicio de consumo de energia

---
+++ src/estudos_em_java01/Exercicios06.java
@@ -0,0 +1,14 @@
+package estudos_em_java01;
+
+public class Exercicios06 {
+    //Declare uma variável contendo o valor do consumo de energia (em kWh). Se for até 100, o preço é 0,50 por kWh. Se for maior que 100, o preço é 0,70 por kWh. Calcule e mostre o valor total.
+    public static void main(String[] args) {
+        double kWh = 102;
+        if (kWh <= 100){
+            kWh = kWh * 0.5;
+        }else {
+            kWh = kWh * 0.7;
+        }
+        System.out.println(kWh);
+    }
+}
