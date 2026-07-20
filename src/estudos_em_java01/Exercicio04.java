ee56e078a16d1961ec22730de2dae0deb2936c59
Grimaldo <grimaldojunior04@gmail.com>
2026-07-19 22:59:29 -0300

    exercicio de quente ou frio

---
+++ src/estudos_em_java01/Exercicio04.java
@@ -0,0 +1,13 @@
+package estudos_em_java01;
+
+public class Exercicio04 {
+    //Declare uma variável contendo um valor de temperatura em graus Celsius e informe se está frio (menor que 20) ou quente (20 ou mais).
+    public static void main(String[] args) {
+        int Celsius = 10;
+        if (Celsius < 20){
+            System.out.println("Frio");
+        }else {
+            System.out.println("Quente");
+        }
+    }
+}
